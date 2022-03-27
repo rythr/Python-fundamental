453433615b9be49dd816b8bfc1ad1015d89fec96
Raki Faturahman <valeclip27@gmail.com>
2022-03-27 09:29:22 +0700

    Sekuensial

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Sintaksis dasar bahasa pemrograman
+1. Sekuensial   : Langkah Berurutan
+2. Percabangan  :Langkah melompat jika kondisi terpenuhi
+3. Perulangan   : mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+"""
+
+#Sequen
+print('Ibu berkata, "Pergi ke toko"')
+print('Budi menjawab, "Baik, apa  yang harus saya lakukan di toko?"')
+print('Ibu berkata, "Beli satu botol susu, jika ada telur beli 6"')
+print('Maka Budi berangkat ketoko')
+print('dan mulai berbelanja')
